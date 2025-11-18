cc8740a996df9f592cec21fea5a3204d825a77ad
Yareli-Borrego-Zapata <23308060610278@cetis61.edu.mx>
2025-11-17 21:05:14 -0700

    Actualizacion

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 app.secret_key = 'clavesecreta'
 
 API = "https://trackapi.nutritionix.com/v2/natural/nutrients"
-info = {
-    "id": "85fe15db",
-    "key": "210735866037a97066bc0cc093fcfb42", 
-    "json": "application/json"
-}
+ID = "85fe15db"
+KEY = "210735866037a97066bc0cc093fcfb42"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
     if request.method == 'POST':
-        id_comida  = request.form['search']
+        id_comida = request.form['search']
+        
+        info = {
+            "x-app-id": ID,
+            "x-app-key": KEY,
+            "x-remote-user-id": "0" 
+        }
         response = requests.post(API, headers=info, json={"query": id_comida})
         if response.status_code == 200:
             result = response.json()
